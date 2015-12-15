da995e53a54bfc62fdb4e167116c601f738016a3
patrick <patrick@debian>
2015-12-15 14:17:13 -0200

    executa a camera sozinho fazendo o streaming

--- teste3.py
+++ teste3.py
@@ -1,10 +1,21 @@
 import sys
+import time
 import scipy
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
 
-ser = serial.Serial(port='/dev/ttyUSB0',baudrate=57600)
+ser = serial.Serial(port='/dev/ttyUSB0',baudrate=115200)
+
+ser.write("RS \r")
+time.sleep(0.5);
+ser.write("FS 1 \r")
+time.sleep(0.5);
+ser.write("SF \r")
+time.sleep(0.1);
+
+def map(x,in_min=16,in_max=240,out_min=0,out_max=250):
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def take(x):
 	if(x==1):
@@ -26,24 +37,22 @@ def takeuntil(x):
 while True :
 	print 'waiting image..'
 	while take(1)!=1:
-		pass		
+		pass
 
 	print 'size'
-	size = take(2)	
+	size = take(2)
 	print size
 
 	image=[]
-	for x in xrange(1,size[0]):
+	for x in xrange(1,87):
 		data = takeuntil(2)
-		image.append([[255-data[i],255-data[i+1],255-data[i+2]] for i in range(0, len(data), 3)])
-	
+		image.append([[255-map(data[i]),255-map(data[i+1]),255-map(data[i+2])] for i in range(0, len(data), 3)])
 
 
-	image=np.array(image)
-	
+
+	#image=np.array(image)
+
 	plt.imshow(image)
 	plt.show(block=False)
 	plt.draw()
  	plt.pause(0.1)
-
- 	

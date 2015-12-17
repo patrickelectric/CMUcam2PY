2a790ce579507ddbb9e99e01e7ff6375405585c1
patrick <patrick@debian>
2015-12-17 15:22:00 -0200

    codigo aparenta estar finalizado

--- sendusb.cpp
+++ sendusb.cpp
@@ -1,36 +1,61 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <string>
+#include <iostream>
 
 int rgbv[]={93, 43, 208, 158, 255, 230};
+char  temp;
+int  i=0;
+char byte[64];
+char msg[9];
+char *pch;
 
 int main()
 {
-    char byte[32];
 
     int fd = open("/dev/ttyUSB0", O_RDWR);
+
+		// restart camera
     write(fd, "RS \r", 4);
 		usleep(500e3);
     read(fd, &byte[0], 32);
     printf("R => %s\n", byte);
-
+		// ask camera to track
 		sprintf(byte, "TC %d %d %d %d %d %d \r",255-rgbv[0],255-rgbv[1],255-rgbv[2],255-rgbv[3],255-rgbv[4],255-rgbv[5]);
-		printf("%s\n",byte );
-		write(fd, byte, 23);
 
+		i=0;
+		temp=0;
+		while(temp!='\r')
+			temp=byte[i++];
+		write(fd, byte, i);
+
+		// proccess data
 		while(1)
 		{
-			char end=0;
-			int i=0;
-			while(end!='\r')
+			i=0;
+			temp=0;
+			while(temp!='\r')
 			{
-				read(fd, &end, 1);
-				byte[i++]=end;
+				read(fd, &temp, 1);
+				byte[i++]=temp;
 			}
 			byte[i]='\0';
-	    printf("R => %s\n", byte);
-		}
 
+			pch = strtok(byte," ");
+			i=0;
+			while(pch != NULL)
+		  {
+				msg[i++]=atoi(pch);
+		    //printf ("%d -> %d\n",i++,msg[i]);
+		    pch = strtok(NULL, " ");
+		  }
+			// Default Type T packet
+			// T mx my x1 y1 x2 y2 pixels confidence
+			printf("coord (%d\t%d)\n",msg[1],msg[2] );
+		}
 
     return 0;
 }
